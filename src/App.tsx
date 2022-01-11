81cdfbe7e43895e9cf020b33def03fbba7ca00ea
Maxim <modorodko@it-one.ru>
2022-01-11 19:11:09 +0300

    commit before eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import css from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css.App}>
+      <header className={css.AppHeader}>
+        <img src={logo} className={css.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={css.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
